5def5fb05368b886191182cde16ef7c30b023e82
Artem Shumajlov <4.artem.0@gmail.com>
2024-04-08 10:13:22 +0300

    update hybrids

--- pages/example/first.ts
+++ pages/example/first.ts
@@ -1,4 +1,4 @@
 import intersect from "https://esm.sh/just-intersect@4.3.0"
-import { define } from "https://esm.sh/hybrids@8.2.14"
+import { define } from "https://esm.sh/hybrids@8.2.15"
 
 export default { intersect, define }
